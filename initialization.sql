ee03dfa1965f89b8f1c55dc3797d7431058c0164
LCT1808 <131013036+LCT1808@users.noreply.github.com>
2024-05-12 23:41:05 +0700

    Create initialization.sql

---
+++ initialization.sql
@@ -0,0 +1,3 @@
+insert into RetailStores values ('S1', 'Store 1', 'HCM', 1)
+insert into Accounts values (1, 'admin', '123456', 'Admin', 0)
+insert into Employees values (1, 'admin', 'admin@gmail.com', 1, 1, null)
